e4134bcf32a4edb7a2f0c29db5030bf3fbb95db5
Woo Jaehyun <wjaehyun97@gmail.com>
2025-10-21 17:05:23 +0900

    sql study

--- 02_sql/05_join.sql
+++ 02_sql/05_join.sql
@@ -1,3 +1,11 @@
+use hr_join;
+
+-- 매번 하나하나 해야해서 너무 많은 셀렉이 필요하다. > 어떻게 하면 한 번에 할 수 있을까?
+select * from emp where emp_id = 100;
+select * from dept where dept_id = 90;
+select * from job where job_id = 'AD_PRES';
+
+
 /* ********************************************************************************
 조인(JOIN) 이란
 - 2개 이상의 테이블에 있는 컬럼들을 합쳐서 가상의 테이블을 만들어 조회하는 방식을 말한다.
@@ -17,7 +25,8 @@
         - 두 테이블의 곱집합을 반환한다. 
 ******************************************************************************** */        
 
-
+-- A, B
+-- from A INNER JOIN B ON A.a = B.b
 
 /* ****************************************
 -- INNER JOIN
@@ -26,31 +35,56 @@ FROM  테이블a INNER JOIN 테이블b ON 조인조건
 - inner는 생략 할 수 있다.
 **************************************** */
 -- 직원의 ID(emp.emp_id), 이름(emp.emp_name), 입사년도(emp.hire_date), 소속부서이름(dept.dept_name)을 조회
-
+-- select emp_id, emp_name, hire_date, dept_id
+-- from emp;
+select emp_id, emp_name, hire_date, dept_name
+from emp e join dept d
+on e.dept_id = d.dept_id;
 
 -- 커미션을(emp.comm_pct) 받는 직원들의 직원_ID(emp.emp_id), 이름(emp.emp_name),
 -- 급여(emp.salary), 커미션비율(emp.comm_pct), 소속부서이름(dept.dept_name), 부서위치(dept.loc)를 조회. 직원_ID의 내림차순으로 정렬.
-
+select e.emp_id, e.emp_name, e.salary, e.comm_pct, d.dept_name, d.loc
+from emp e join dept d
+on e.dept_id = d.dept_id
+where e.comm_pct is not null;
 
 -- 직원의 ID(emp.emp_id)가 100인 직원의 직원_ID(emp.emp_id), 이름(emp.emp_name), 입사년도(emp.hire_date), 소속부서이름(dept.dept_name)을 조회.
-
+select e.emp_id, e.emp_name, e.hire_date, d.dept_name
+from emp e join dept d
+on e.dept_id = d.dept_id
+where e.emp_id = 100;
 
 --  직원 ID 가 200 인 직원의 직원_ID(emp.emp_id), 이름(emp.emp_name), 급여(emp.salary), 담당업무명(job.job_title), 소속부서이름(dept.dept_name)을 조회              
-
+select e.emp_id, e.emp_name, e.salary, j.job_title, d.dept_name
+from emp e join job j on e.job_id = j.job_id
+		   join dept d on e.dept_id = d.dept_id
+where emp_id = 200;
 
 -- 부서_ID(dept.dept_id)가 30인 부서의 이름(dept.dept_name), 위치(dept.loc), 그 부서에 소속된 직원의 이름(emp.emp_name)을 조회.
-
+select d.dept_name, d.loc, e.emp_name
+from emp e join dept d on e.dept_id = d.dept_id
+where d.dept_id = 30;
 
 -- 직원의 ID(emp.emp_id), 이름(emp.emp_name), 급여(emp.salary), 급여등급(salary_grade.grade) 를 조회. 
-
-
+select e.emp_id, e.emp_name, e.salary, concat(s.grade, '등급') grade
+from   emp e join salary_grade s on e.salary between s.low_sal and s.high_sal;
 
 -- 부서별 급여(salary)의 평균을 조회. 부서이름(dept.dept_name)과 급여평균을 출력. 급여 평균이 높은 순서로 정렬. 
-
-
+select d.dept_id, d.dept_name, avg(e.salary) "급여평균"
+from emp e join dept d on e.dept_id = d.dept_id
+group by d.dept_id, d.dept_name
+order by 3 desc;
 
 -- 직원의 ID(emp.emp_id), 이름(emp.emp_name), 업무명(job.job_title), 급여(emp.salary), 급여등급(salary_grade.grade), 소속부서명(dept.dept_name)을 조회.
-
+select e.emp_id,
+	   e.emp_name,
+       j.job_title,
+       e.salary,
+       s.grade,
+       d.dept_name
+from   emp e join job j on e.job_id = j.job_id
+			 join salary_grade s on e.salary between s.low_sal and s.high_sal
+             join dept d on e.dept_id = d.dept_id;
 
 /* ****************************************************
 Self 조인
@@ -58,8 +92,11 @@ Self 조인
 **************************************************** */
 
 -- 직원 ID가 101인 직원의 직원의 ID(emp.emp_id), 이름(emp.emp_name), 상사이름(emp.emp_name)을 조회
-
-
+select e1.emp_id "직원 ID",
+	   e1.emp_name "직원 이름",
+       e2.emp_name "상사 이름"
+from emp e1 join emp e2 on e1.mgr_id = e2.emp_id
+where e1.emp_id = 101;
 
 /* ****************************************************************************
 외부 조인 (Outer Join)
@@ -76,38 +113,40 @@ from 테이블a [LEFT | RIGHT] OUTER JOIN 테이블b ON 조인조건
 
 **************************************************************************** */
 
-
 -- 직원의 id(emp.emp_id), 이름(emp.emp_name), 급여(emp.salary), 부서명(dept.dept_name), 부서위치(dept.loc)를 조회. 
 -- 부서가 없는 직원의 정보도 나오도록 조회. dept_name의 내림차순으로 정렬한다.
-
-
-
+select e.emp_id, e.emp_name, e.salary, d.dept_name, d.loc
+-- from   emp e right join dept d on e.dept_id = d.dept_id -- 오른쪽 중심 dept
+from   emp e left join dept d on e.dept_id = d.dept_id -- 왼쪽 중심 emp 있으면 정보 없어도 dept 출력
+order by d.dept_name desc;
 
 -- 모든 직원의 id(emp.emp_id), 이름(emp.emp_name), 부서_id(emp.dept_id)를 조회하는데
 -- 부서_id가 80 인 직원들은 부서명(dept.dept_name)과 부서위치(dept.loc) 도 같이 출력한다. (부서 ID가 80이 아니면 null이 나오도록)
-
-
-        
+select e.emp_id, e.emp_name, e.dept_id, d.dept_name, d.loc
+from emp e left join dept d on e.dept_id = d.dept_id and d.dept_id = 80
+order by d.dept_id desc;
+	
 --  직원_id(emp.emp_id)가 100, 110, 120, 130, 140인 직원의 ID(emp.emp_id),이름(emp.emp_name), 업무명(job.job_title) 을 조회. 업무명이 없을 경우 '미배정' 으로 조회
-
-
+select e.emp_id, e.emp_name, ifnull(j.job_title, "미배정") "업무명"
+from   emp e left join job j on e.job_id = j.job_id
+where  emp_id in (100, 110, 120, 130, 140);
 
 -- 부서 ID(dept.dept_id), 부서이름(dept.dept_name)과 그 부서에 속한 직원들의 수를 조회. 직원이 없는 부서는 0이 나오도록 조회하고 직원수가 많은 부서 순서로 조회.
-
-
+select d.dept_id, d.dept_name, count(emp_id) "직원 수" -- 직원 수 구할 때 내가 세려고 하는 테이블의 primary key로 count하는게 실수를 줄인다.
+from dept d left join emp e on d.dept_id = e.dept_id
+group by d.dept_id, d.dept_name
+order by 3 desc;
 
 -- EMP 테이블에서 부서_ID(emp.dept_id)가 90 인 모든 직원들의 id(emp.emp_id), 이름(emp.emp_name), 상사이름(emp.emp_name), 입사일(emp.hire_date)을 조회. 
 -- 입사일은 yyyy/mm/dd 형식으로 출력
-
+select e.emp_id, e.emp_name "직원이름", m.emp_name "상사이름", date_format(e.hire_date, '%Y/%m/%d') "입사일"
+from emp e left join emp m on e.mgr_id = m.emp_id
+where e.dept_id = 90;
 
 -- 2003년~2005년 사이에 입사한 모든 직원의 id(emp.emp_id), 이름(emp.emp_name), 업무명(job.job_title), 급여(emp.salary), 입사일(emp.hire_date),
 -- 상사이름(emp.emp_name), 상사의입사일(emp.hire_date), 소속부서이름(dept.dept_name), 부서위치(dept.loc)를 조회.
-
-
-
-
-
-
-
-
-
+select e.emp_id, e.emp_name "직원 이름", j.job_title, e.salary, e.hire_date"직원 입사일", m.emp_name "상사이름", m.hire_date"상사 입사일", d.dept_name, d.loc
+from emp e left join job j on e.job_id = j.job_id
+		   left join dept d on d.dept_id = e.dept_id
+           left join emp m on e.mgr_id = m.emp_id
+where year(e.hire_date) between '2003' and '2005'; 
--- 02_sql/table생성/emp_join.sql
+++ 02_sql/table생성/emp_join.sql
@@ -1,7 +1,7 @@
--- CREATE DATABASE hr_join;
--- USE  hr_join;
+CREATE DATABASE hr_join;
+USE  hr_join;
 
--- set foreign_key_checks=0;
+set foreign_key_checks=0;
 
 -- ------------------------------------------------------
 -- DDL FOR DEPT
@@ -93,10 +93,11 @@ CREATE TABLE emp(
     salary 	  DECIMAL(7,2) NOT NULL,
     comm_pct  DECIMAL(2,2),
     dept_id   INT,
+    -- constraint pk_emp primary key(emp_id),
     CONSTRAINT fk_emp_dept FOREIGN KEY(dept_id) REFERENCES dept(dept_id) ON DELETE SET NULL,
     CONSTRAINT fk_emp_job FOREIGN KEY(job_id) REFERENCES job(job_id) ON DELETE SET NULL,
     CONSTRAINT fk_emp_mgr FOREIGN KEY(mgr_id) REFERENCES emp(emp_id) ON DELETE SET NULL
-);
+);	-- on delete cascade
 
 INSERT INTO emp (emp_id,emp_name,job_id,mgr_id,hire_date,salary,comm_pct,dept_id) VALUES (100,'Steven','AD_PRES',null,CAST('03-06-17' AS DATE),24000,null,90);
 INSERT INTO emp (emp_id,emp_name,job_id,mgr_id,hire_date,salary,comm_pct,dept_id) VALUES (101,'Neena','AD_VP',100,CAST('05-09-21' AS DATE),17000,null,90);
@@ -223,3 +224,13 @@ INSERT INTO salary_grade (grade,low_sal,high_sal) VALUES (2, 5001, 10000);
 INSERT INTO salary_grade (grade,low_sal,high_sal) VALUES (3, 10001, 15000);
 INSERT INTO salary_grade (grade,low_sal,high_sal) VALUES (4, 15001, 20000);
 INSERT INTO salary_grade (grade,low_sal,high_sal) VALUES (5, 20001, 99999);
+
+use hr_join;
+select * from dept;
+select * from job;
+select * from emp;
+--  (emp_id,emp_name,job_id,mgr_id,hire_date,salary,comm_pct,dept_id)
+select * from salary_grade;
+
+select * from emp order by emp_id desc;
+
